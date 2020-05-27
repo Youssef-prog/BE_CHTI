43604b168cf25f402a2180d82f5e0c36c98e4770
Youssef-prog <youssefamari7@gmail.com>
2020-05-27 13:06:51 +0200

    Implémentation module

--- PROJET ASM/Obj 1/PROJ_UN-Etape2-VirguleFixe/Src/module.s
+++ PROJET ASM/Obj 1/PROJ_UN-Etape2-VirguleFixe/Src/module.s
@@ -1,1 +1,78 @@
 	;Area, moncode, code
+	AREA monconde, code
+		
+	export M2
+
+	import TabCos
+	import TabSin
+	import dft
+
+
+M2 proc ; r0 = @TabSig, r1 = k
+
+
+	push {lr}
+	push {r4}
+	push {r5}
+	push {r6}
+	push {r7}
+
+
+	ldr r2, =TabSin ; r2 = @TabSin
+	ldr r3, =TabCos ; r3 = @TabCos
+
+
+	push {r0}
+	push {r1}
+	push {r2}
+	push {r3}
+
+	mov r0, r1
+	mov r1, r2
+	mov r2, r0
+	bl dft
+
+	mov r4, r0 ; r4 = tab des im
+
+	pop {r3}
+	pop {r2}
+	pop {r1}
+	pop {r0}
+
+	push {r0}
+	push {r1}
+	push {r2}
+	push {r3}
+
+	mov r0, r1
+	mov r1, r3
+	mov r2, r0
+	bl dft
+
+	mov r5, r0 ; r5 = tab des re
+
+	pop {r3}
+	pop {r2}
+	pop {r1}
+	pop {r0}
+
+
+	ldrsh r6, [r4, r1, lsl #1]
+	ldrsh r7, [r5, r1, lsl #1]
+
+
+	mul r6, r6 ; im2
+
+	mla r7, r7, r7, r6 ;M2
+
+	mov r0, r7
+
+	pop {r7}
+	pop {r6}
+	pop {r5}
+	pop {r4}
+	pop {pc}
+
+	endp
+
+	end
--- PROJET ASM/Obj 1/PROJ_UN-Etape2-VirguleFixe/Src/principal.c
+++ PROJET ASM/Obj 1/PROJ_UN-Etape2-VirguleFixe/Src/principal.c
@@ -1,18 +1,23 @@
 extern void sommeCarre(void);
 extern int dft(int, int**, int**);
+extern int M2(int**, int);
 extern int* TabSig;
 extern int* TabSin;
 extern int* TabCos;
 
 int im[64], re[64];
+int m2;
 
 int main(void){ // faire boucle for
-    
-    for(int k=0; k<64; k++){
-	im[k] = dft(k, &TabSin, &TabSig);
-	re[k] = dft(k, &TabCos, &TabSig);
-	//int m2 = im*im + re*re;
-    }
-    
-    while(1){}
+
+	/*for(int k=0; k<64; k++){
+		im[k] = dft(k, &TabSin, &TabSig);
+		re[k] = dft(k, &TabCos, &TabSig);
+
+	}*/
+
+	int k = 23;
+	m2 = M2(&TabSig, k);
+
+	while(1){}
 }
